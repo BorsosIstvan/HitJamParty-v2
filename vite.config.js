894c747a491089aa9c4a7fcc5e71e91323189b40
BorsosIstvan <bistvanus@gmail.com>
2026-08-26 20:24:29 +0200

    ci: add automatic GitHub Pages deployment workflow

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vite.dev
 export default defineConfig({
   plugins: [react()],
+  base: 'https://github.com/BorsosIstvan/HitJamParty-v2.git', // HIDEG: Ide írd be az új repód pontos nevét két perjel közé!
 })
+
