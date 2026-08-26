ee7c204f47f62bcf5d8e8f716386bfa46d7081b7
BorsosIstvan <bistvanus@gmail.com>
2026-08-26 20:32:30 +0200

    fix: remove .git suffix from vite config base path

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/BorsosIstvan/HitJamParty-v2', // HIDEG: Ide írd be az új repód pontos nevét két perjel közé!
+  base: 'HitJamParty-v2', // HIDEG: Ide írd be az új repód pontos nevét két perjel közé!
 })
 
